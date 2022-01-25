421dadab1f7358c75ace37455fea0354510be1bd
Igor Rafael <igor_rafa_lima@hotmail.com>
2022-01-25 18:28:22 -0300

    adicionado novos arquivos

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Node rodando com sucesso')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 5000
+const userRoute = require('./routes/userRoute');
+
+userRoute(app)
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get("/", function(req, res){
+    res.send("Ola mundo pelo express")
+})
+
+
+
+app.listen(port, () => console.log('API rodando na porta 5000'))
